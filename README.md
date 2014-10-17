11fb0e5c75f3390197ff50c0cfbd15ac24d0f347
James Young <jmeyoung@gmail.com>
2014-10-17 16:53:02 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Directory Reader
+Reads the directory and returns a directory listing for a specific directory.
+
+
+## How to set the directory it reads
+Use the `DIRECTORY_READER_PATH` environment variable.
+
+
+## How to deploy
+```bash
+$ git remote add dokku dokku@runn.io:directory-reader
+$ git push dokku master
+```
---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var fs = require('fs'),
+    restify = require('restify'),
+    packageConfig = require('./package.json'),
+    port = process.env.PORT || '8080',
+    server = restify.createServer({name: 'Directory Reader'});
+
+function getPath(request, response, next) {
+    var path = process.env.DIRECTORY_READER_PATH || '.',
+        files;
+
+    if (fs.existsSync(path)) {
+        files = fs.readdirSync(path);
+        response.send(200, files);
+    } else {
+        response.send(500, {error: 'path does not exist'});
+    }
+}
+
+function healthcheck(request, response, next) {
+    response.send(200, {'package': packageConfig, env: process.env});
+}
+
+
+server.get('/api/latest/path', getPath);
+server.head('/api/latest/path', getPath);
+
+server.get('/_healthcheck', healthcheck);
+
+server.listen(port, function () {
+    console.log('%s listening at %s', server.name, server.url);
+});
